1cc1f4da27271d4a659f30189ad9f8ec49a1636d
soorajk1 <ksooraj162@gmail.com>
2023-11-06 13:56:24 +0530

    created upsert commands

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,99 +2,105 @@
 const { PrismaClient } = require('@prisma/client');
 
 // Import your sample data
-const sampleData = require('./sampledata2'); // Adjust the path if necessary
+const sampleData = require('../fixtures/sampledata'); // Adjust the path if necessary
 
 async function seed() {
   const prisma = new PrismaClient();
-
+  // const transaction = await prisma.$transaction();
   try {
-    // Insert sample data into the database
-
     for (const techSociety of sampleData.technicalSocieties) {
-      await prisma.technicalSociety.create({
-        data: techSociety,
+      await prisma.technicalSociety.upsert({
+        where: { id: techSociety.id },
+        create: techSociety,
+        update: {},
       });
     }
 
     for (const user of sampleData.users) {
-      await prisma.user.create({
-        data: user,
+      await prisma.user.upsert({
+        where: { id: user.id },
+        create: user,
+        update: {},
       });
     }
 
-
-    for (const techSociety of sampleData.eventCategories) {
-      await prisma.eventCategories.create({
-        data: techSociety,
+    for (const eventCategory of sampleData.eventCategories) {
+      await prisma.eventCategories.upsert({
+        where: { id: eventCategory.id },
+        create: eventCategory,
+        update: {},
       });
     }
 
-
-    for (const techSociety of sampleData.eventCoordinators) {
-      await prisma.eventCoordinators.create({
-        data: techSociety,
+    for (const eventCoordinator of sampleData.eventCoordinators) {
+      await prisma.eventCoordinators.upsert({
+        where: { id: eventCoordinator.id },
+        create: eventCoordinator,
+        update: {},
       });
     }
 
     for (const event of sampleData.events) {
-      await prisma.events.create({
-        data: event,
+      await prisma.events.upsert({
+        where: { id: event.id },
+        create: event,
+        update: {},
       });
     }
 
-    for (const techSociety of sampleData.eventDoubts) {
-      await prisma.EventDoubts.create({
-        data: techSociety,
+    for (const eventDoubt of sampleData.eventDoubts) {
+      await prisma.EventDoubts.upsert({
+        where: { id: eventDoubt.id },
+        create: eventDoubt,
+        update: {},
       });
     }
 
-
-    for (const techSociety of sampleData.sponsorCategories) {
-      await prisma.sponsorCategories.create({
-        data: techSociety,
+    for (const sponsorCategory of sampleData.sponsorCategories) {
+      await prisma.sponsorCategories.upsert({
+        where: { id: sponsorCategory.id },
+        create: sponsorCategory,
+        update: {},
       });
     }
 
+    for (const sponsor of sampleData.sponsor) {
+      await prisma.Sponsor.upsert({
+        where: { id: sponsor.id },
+        create: sponsor,
+        update: {},
+      });
+    }
 
-      for (const techSociety of sampleData.sponsor) {
-        await prisma.Sponsor.create({
-          data: techSociety,
-        });
-      }
+    for (const techSociety of sampleData.lectures) {
+      await prisma.Lectures.upsert({
+        where: { id: techSociety.id },
+        create: techSociety,
+        update: {},
+      });
+    }
 
-      for (const techSociety of sampleData.lectures) {
-        await prisma.Lectures.create({
-          data: techSociety,
-        });
-      }
+    for (const techSociety of sampleData.faqs) {
+      await prisma.FAQs.upsert({
+        where: { id: techSociety.id },
+        create: techSociety,
+        update: {},
+      });
+    }
 
-      for (const techSociety of sampleData.faqs) {
-        await prisma.FAQs.create({
-          data: techSociety,
-        });
-      }
-      for (const techSociety of sampleData.announcements) {
-        await prisma.announcements.create({
-          data: techSociety,
-        });
-      }
-    // Insert data for other models in a similar way
-    const temp = await prisma.user.findMany({
-        select:{
-            name: true,
-            email: true,
-            mobile: true,
-        }
-    })
+    for (const techSociety of sampleData.announcements) {
+      await prisma.announcements.upsert({
+        where: { id: techSociety.id },
+        create: techSociety,
+        update: {},
+      });
+    }
 
-    console.log(temp);
     console.log('Sample data has been inserted successfully.');
   } catch (error) {
+    // await transaction.$rollback();
     console.error('Error seeding the database:', error);
-  } finally {
-    await prisma.$disconnect();
   }
 }
 
-// Call the seed function to insert the data
 seed();
